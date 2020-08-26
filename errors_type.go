b84cb0cc7cc82098af478d4a21555dbde92f4803
victorneuret <victor.neuret@gmail.com>
2020-08-26 23:40:41 +0900

    feat: Add Login error type

--- errors_type.go
+++ errors_type.go
@@ -5,9 +5,11 @@ type ErrorType string
 const (
 	Email    ErrorType = "email"
 	Required ErrorType = "required"
+	Login    ErrorType = "login"
 )
 
 var ErrorTypes = map[ErrorType]string{
 	Email:    "The email given is not correct",
 	Required: "The field is required",
+	Login:    "The login is incorrect",
 }
