774356038ca002f9940f4874a6232c41252918e3
victorneuret <victor.neuret@gmail.com>
2020-09-16 21:35:18 +0900

    Add KafkaErrorMessage struct

--- data.go
+++ data.go
@@ -8,3 +8,8 @@ type field struct {
 	Name   string `json:"name"`
 	Reason string `json:"reason"`
 }
+
+type KafkaErrorMessage struct {
+	Code    int    `json:"code"`
+	Content []byte `json:"content"`
+}
